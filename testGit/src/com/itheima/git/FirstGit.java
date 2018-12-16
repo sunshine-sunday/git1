43553aa1f50a049d805d270e4edffa148d72992a
lkx <992915924@qq.com>
2018-12-16 21:39:53 +0800

    上传test文件

---
+++ testGit/src/com/itheima/git/FirstGit.java
@@ -0,0 +1,8 @@
+package com.itheima.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("this is new git test");
+        System.out.println("hello world");
+    }
+}
